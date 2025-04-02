fb3feaabd34962c7a7373f25b3418fa329659bea
sharjeel1999 <sharjeelmasood10b15@gmail.com>
2025-04-02 16:18:07 +0900

    cleanup

--- custom_testing.py
+++
@@ -1,9 +0,0 @@
-from torchvision.models import resnet50, vit_b_16
-
-from view.torchview import draw_graph
-
-model = vit_b_16()
-# print(model)
-model_graph = draw_graph(model, input_size = (2, 3, 224, 224), expand_nested = True, save_graph = True, depth = 4, hide_inner_tensors=True)
-model_graph.visual_graph
-print('done')
