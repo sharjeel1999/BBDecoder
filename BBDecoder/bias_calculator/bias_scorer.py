b5462f8c3ab15863490798cfb28782f926730450
sharjeel1999 <sharjeelmasood10b15@gmail.com>
2025-09-24 14:18:07 +0500

    additions

--- BBDecoder/bias_calculator/bias_scorer.py
+++ BBDecoder/bias_calculator/bias_scorer.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision.transforms.v2 as transforms
 
 from pytorch_grad_cam import (
     GradCAM,
@@ -12,7 +13,7 @@ from pytorch_grad_cam import (
     LayerCAM,
 )
 class BiasScorer:
-    def __init__(self, model, test_loader, bias_calculator, device):
+    def __init__(self, model, test_loader, bias_calculator, device, resize_attentions):
         self.cams = {
             "GradCAM": GradCAM,
             "HiResCAM": HiResCAM,
@@ -26,10 +27,25 @@ class BiasScorer:
         }
         self.model = model
         self.bias_calculator = bias_calculator
-        self.bias_calculator
+        self.device = device
+        self.resize_attentions = resize_attentions
 
     def get_maps(self, image, targets, target_layers):
-        cam = self.cams[self.bias_calculator.cam_type](model = self.model, target_layers = target_layers)
+        cam = self.cams[self.bias_calculator](model = self.model, target_layers = target_layers)
 
         attention_map = torch.tensor(cam(input_tensor = image, targets = targets), device=self.device)
+
+        if self.resize_attentions:
+            attentions = transforms.functional.resize(
+                torch.squeeze(attentions),
+                size = image.shape[-2:],
+                interpolation = transforms.InterpolationMode.BILINEAR,
+            )
+        else:
+            attentions = torch.squeeze(attentions)
         
+        attentions -= attentions.min(1)[0].min(1)[0].unsqueeze(1).unsqueeze(1)
+        attentions /= 1e-7 + attentions.max(1)[0].max(1)[0].unsqueeze(1).unsqueeze(1)
+        attentions = torch.clamp(attentions, min=0, max=1)
+        
+        return attentions
